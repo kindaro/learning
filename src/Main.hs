3eafe83d9032416a4c37364461cd8e029621ed4e
Ignat Insarov <kindaro@gmail.com>
2015-09-24 17:09:15 +0300

    Compute inner product of two vectors (lists).

--- src/Main.hs
+++ src/Main.hs
@@ -4,10 +4,15 @@ module Main where
 data StdObj x y = StdObjA x | StdObjB y deriving Show
 
 main :: IO ()
-main = putStrLn (show $ obj)
-
+main = putStrLn . show $ test
 
 obj :: StdObj Integer Integer
 obj = StdObjA 1
 
+test = innerProduct v1 v2 == r
+    where
+        v1 = [1, 2, 3]
+        v2 = [4, 5, 6]
+        r = 32
 
+innerProduct = (sum .) . zipWith (*)
