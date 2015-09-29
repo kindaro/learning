dc855bcda131967f74a9f575f0839693fa2a176e
Ignat Insarov <kindaro@gmail.com>
2015-09-29 04:02:15 +0300

    Three ways to do the same, set alongside for your review.

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,13 @@
 module Main where
 
 
-main = putStrLn.show $ solution f list == ["uber Amy", "uber uber Henrik", "uber uber uber Josephine"]
+import Data.List
+
+
+main = do
+    putStrLn.show $ solution f list
+    putStrLn.show $ solution' f list
+    putStrLn.show $ solution3 f list
 
 
 f = (++) "uber "
@@ -11,3 +17,14 @@ list = ["Amy", "Henrik", "Josephine"]
 solution :: (String -> String) -> [String] -> [String]
 solution _ [] = []
 solution f (l:ls) = map f $ l : solution f ls
+
+solution' :: (String -> String) -> [String] -> [String]
+solution' _ [] = []
+solution' f (l:ls) = (head $ map f [l]) : (map f $ solution' f ls)
+
+recursive :: (a -> a) -> (a -> a) -> [a] -> [a]
+recursive _ _ [] = []
+recursive fh ft (l:ls) = fh l : map ft (recursive fh ft ls)
+
+solution3 :: (String -> String) -> [String] -> [String]
+solution3 f = recursive f f
