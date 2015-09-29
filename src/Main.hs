c5a12fa33027346c2837ee3c16180009df70b671
Ignat Insarov <kindaro@gmail.com>
2015-09-29 19:57:25 +0300

    Count and compute the sum of digits in decimal representation of a given number.

--- src/Main.hs
+++ src/Main.hs
@@ -1,30 +1,13 @@
 module Main where
 
 
-import Data.List
+main = getLine >>= putStrLn . show . f . read
 
+f :: Integer -> (Integer, Integer)
+f x = (sum digits, (toInteger.length) digits)
+    where digits = digits' 10 x
 
-main = do
-    putStrLn.show $ solution f list
-    putStrLn.show $ solution' f list
-    putStrLn.show $ solution3 f list
-
-
-f = (++) "uber "
-
-list = ["Amy", "Henrik", "Josephine"]
-
-solution :: (String -> String) -> [String] -> [String]
-solution _ [] = []
-solution f (l:ls) = map f $ l : solution f ls
-
-solution' :: (String -> String) -> [String] -> [String]
-solution' _ [] = []
-solution' f (l:ls) = (head $ map f [l]) : (map f $ solution' f ls)
-
-recursive :: (a -> a) -> (a -> a) -> [a] -> [a]
-recursive _ _ [] = []
-recursive fh ft (l:ls) = fh l : map ft (recursive fh ft ls)
-
-solution3 :: (String -> String) -> [String] -> [String]
-solution3 f = recursive f f
+digits' :: Integer -> Integer -> [Integer]
+digits' _ 0 = []
+digits' base n = snd q : digits' base (fst q)
+    where q = quotRem n base
