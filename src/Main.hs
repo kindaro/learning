34d0d087121f0fb6dccb09fd80c819871ca65a2e
Ignat Insarov <kindaro@gmail.com>
2015-10-09 04:01:54 +0300

    Enhance: More GADTs understanding.

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,17 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
 module Main where
 
 
-data StdObj x y = StdObjA {x :: Integer} | StdObjB {y :: Integer} deriving Show
+data StdObj = StdObjA {x :: Integer} | StdObjB {y :: Integer} deriving Show
+
+data Elem a i where
+    Elem :: Eq i => a -> i -> Elem a i
+
+deriving instance (Show a, Show i) => Show (Elem a i)
+
+
 
 main :: IO ()
-main = putStrLn (show $ StdObjA 1)
+main = putStrLn (show $ Elem 1 2)
